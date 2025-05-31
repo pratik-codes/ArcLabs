3c46ecf049c4295445b611ef5e362992dfc447f8
Pratik <pratik2018id@gmail.com>
2025-06-01 04:33:08 +0530

    added favicon

--- src/data/work.json.ts
+++ src/data/work.json.ts
@@ -5,19 +5,19 @@ export interface Template {
     type: string;
   };
   const dashlit: Template = {
-    url: "https://dashlit.app/",
+    url: "https://dashlit.byarclabs.com/",
     description: "An open source productivity extension for your browser. Imagine a browsing experience where seamless organization meets enhanced productivity.",
     type: "Productivity Tool",
     title: "Dashlit",
   };
   const sifu: Template = {
-    url: "https://sifu.solana.com/",
+    url: "https://sifusecurity.com/",
     description: "Real-time fraud prevention for Solana smart contracts secure every transaction, on-chain and beyond",
     type: "Blockchain Security",
-    title: "Sifu (Safe Interactions for Users)",
+    title: "SIFU - Safe Interactions for Users",
   };
   const solpass: Template = {
-    url: "https://solpass.app/",
+    url: "https://solpass.byarclabs.com/",
     description: "Manage your passwords with unmatched security and privacy on the Solana blockchain. Experience the future of password management today.",
     type: "Blockchain Application",
     title: "Solpass",
